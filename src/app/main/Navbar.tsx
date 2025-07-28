f50e483025869b5dbceffbf7dc789367853576ee
leandrodalbo <leandro.edbo@gmail.com>
2025-07-28 11:53:02 -0300

    fixed navbar

--- src/app/main/Navbar.tsx
+++ src/app/main/Navbar.tsx
@@ -1,7 +1,11 @@
 const Navbar = () => {
   return (
     <nav className="bg-brand-orange text-black px-6 py-4 shadow-md flex justify-between items-center">
-      <div className="text-xl font-bold text-white">Before You Bet</div>
+      <div className="text-xl font-bold text-white">
+        <a href="/" className="hover:text-black">
+          Before You Bet
+        </a>
+      </div>
       <div className="space-x-4 text-sm">
         <a href="/" className="hover:text-white">
           Home
